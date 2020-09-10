6e9b98566978f69afaed83104c23f6d3219dbd55
Manikandan Dhanapal <manikandanthedeveloper@gmail.com>
2020-09-10 21:43:57 +0530

    Implementing complex operation for seller

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,10 @@ Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' =>
     Sellers
 */
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
+Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create', 'show', 'edit']]);
 
 /*
     Categories
