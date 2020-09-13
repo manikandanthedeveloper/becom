5901b2e1e541175fe62c1bf6a6ee3ef30525bff1
Manikandan Dhanapal <manikandanthedeveloper@gmail.com>
2020-09-13 10:39:58 +0530

    Sending email for users accounts verification

--- routes/api.php
+++ routes/api.php
@@ -61,3 +61,5 @@ Route::resource('transactions.sellers', 'Transaction\TransactionSellerController
     Users
 */
 Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
+Route::get('users/verify/{token}', 'User\UserController@verify')->name('verify');
+Route::get('users/{user}/resend', 'User\UserController@resend')->name('resend');
