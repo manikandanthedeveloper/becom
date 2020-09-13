a905484883f91b8baae1d4f844361407257d39b3
Manikandan Dhanapal <manikandanthedeveloper@gmail.com>
2020-09-14 01:42:02 +0530

    Preparing the api for user authentication using sessions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
@@ -97,6 +98,10 @@ class Handler extends ExceptionHandler
             }
         }
 
+        if ($exception instanceof TokenMismatchException) {
+            return redirect()->back()->withInput($request->input());
+        }
+
         if(config('app.debug')) {
             return parent::render($request, $exception);
         }
@@ -131,10 +136,17 @@ class Handler extends ExceptionHandler
     {
         $errors = $e->validator->errors()->getMessages();
 
+        if ($this->isFrontend($request)) {
+            return $request->ajax() ? response()->json($errors, 422) : redirect()
+                ->back()
+                ->withInput($request->input())
+                ->withErrors($errors);
+        }
+
         return $this->errorResponse($errors, 422);
     }
 
     private function isFrontend($request) {
-        return $request->acceptsHtml() && collect($request->round()->middleware())->contains('web');
+        return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
     }
 }
